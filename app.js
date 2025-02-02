e1cc3bb02d3aea4c3862007b3024e65e3233ec17
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-02-02 20:23:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require("cors")
+const { getFact, isPrime, isPerfect, digit_sum, properties } = require('./helper')
+const { error } = require('console')
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/classify-number/", async(req, res) =>{
+    try{
+    const no = req.query.number
+
+    if(!no){
+        return res.status(400).json({
+            error: true,
+            message: "missing number"
+        })
+    }
+    const number = Number(no)
+
+    if(isNaN(number)){
+        return res.status(400).json({
+            number: "alphabet",
+            error: true
+    })
+}
+
+    return res.status(200).json({
+        number: number,
+        is_prime: isPrime(number),
+        is_perfect: isPerfect(number),
+        properties: properties(number),
+        digit_sum: digit_sum(number),
+        fun_fact: await getFact(number)
+    })
+}
+    catch(error){
+        res.status(500).json({
+            error: true,
+            message: 'Internal Server error'
+        })
+    }
+})
+
+
+app.use("**", (req, res) =>{
+    res.status(404).json({
+        error: true,
+        message: "resource does not exist"
+    })
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ helper.js
@@ -0,0 +1,81 @@
+const axios = require('axios')
+const { config } = require('dotenv')
+config()
+
+const { NUMBERS_BASE_URL } = process.env
+
+const base_uri = NUMBERS_BASE_URL
+
+exports.getFact = async(number) =>{
+    try{
+    const url = `${base_uri}/${number}/math`
+
+    const response = await axios.get(url)
+    return response.data
+    }
+
+    catch(error){
+        console.log(error.message)
+        return false
+    }
+
+
+}
+
+exports.isPrime = (number) => {
+    if (number <= 1) return false
+    let i = 2
+    while(i< number - 1){
+        if(number % i === 0){
+            return false
+        }
+        i++
+    }
+
+    return true
+}
+
+exports.isPerfect = (number) => {
+    if (number <= 1) return false
+    let sum = 0
+    let i = 1
+
+    while(i <= number/2){
+        if(number % i === 0){
+            sum+=i
+        }
+        i++
+    }
+
+    return sum === number
+}
+
+exports.digit_sum = (number) => {
+    const digits = String(number).split('').map(Number)
+    let sum = 0
+    for( let i = 0; i< digits.length; i++){
+        sum+=digits[i]
+    }
+    return sum
+}
+
+exports.properties = (number) =>{
+    let properties = []
+    const digits = String(number).split('').map(Number)
+    const base = digits.length
+    let sum = 0
+    for(let i = 0; i< digits.length; i++){
+        sum = sum + (digits[i] ** base)
+    }
+    if (sum === number){
+        properties.push("armstrong")
+    }
+
+    if(number % 2 === 0){
+        properties.push("even")
+    }
+    else{
+        properties.push("odd")
+    }
+    return properties
+}
